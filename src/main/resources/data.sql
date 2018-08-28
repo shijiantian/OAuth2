3f5331ad5bed4e0d7487d2d5e8e240befdecff9c
shijiantian <2423939309@qq.com>
2018-08-28 20:20:49 +0800

    添加时间戳字段

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-replace INTO user VALUES (1,'大明','123456'),(2,'小明','123456');
-replace INTO role VALUES (1,'admin'),(2,'user');
-replace INTO user_role VALUES (1,1,1),(2,2,2);
+replace INTO user(id,name,password) VALUES (1,'大明','123456'),(2,'小明','123456');
+replace INTO role(id,name) VALUES (1,'admin'),(2,'user');
+replace INTO user_role(id,user_id,role_id) VALUES (1,1,1),(2,2,2);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,16 +2,22 @@
     id bigint(20) not NULL auto_increment,
     name VARCHAR(8) not null,
     password VARCHAR(64) not null,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     primary key(id)
 )engine=InnoDB;
 create table if not exists role(
     id bigint(20) not NULL auto_increment,
     name VARCHAR(8) not null,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     primary key(id)
 )engine=InnoDB;
 create table if not exists user_role(
     id bigint(20) not NULL auto_increment,
     user_id bigint(20) not NULL,
     role_id bigint(20) not NULL,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     primary key(id)
 )engine=InnoDB;
