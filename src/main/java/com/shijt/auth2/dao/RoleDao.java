cd68eefc02d6047dfb5a36322ab1cd8c3a99af00
shijiantian <2423939309@qq.com>
2018-08-29 15:29:33 +0800

    添加数据库操作

---
+++ src/main/java/com/shijt/auth2/dao/RoleDao.java
@@ -0,0 +1,8 @@
+package com.shijt.auth2.dao;
+
+import com.shijt.auth2.vo.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleDao extends CrudRepository<Role,Long> {
+
+}
---
+++ src/main/java/com/shijt/auth2/dao/UserRoleDao.java
@@ -0,0 +1,8 @@
+package com.shijt.auth2.dao;
+
+import com.shijt.auth2.vo.UserRole;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRoleDao extends CrudRepository<UserRole,Long> {
+
+}
