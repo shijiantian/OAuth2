6df08a6bfc3422a7789eb5168f64306669520df7
shijiantian <2423939309@qq.com>
2018-08-29 16:59:00 +0800

    将密码加密

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-replace INTO user(id,name,password) VALUES (1,'大明','123456'),(2,'小明','123456');
+replace INTO user(id,name,password) VALUES (1,'大明','$2a$10$0hGefzpJErwu8b2yLbgD.OE3/WNypV15UzD785e5a82hXtqyYZR9q'),(2,'小明','$2a$10$0hGefzpJErwu8b2yLbgD.OE3/WNypV15UzD785e5a82hXtqyYZR9q');
 replace INTO role(id,name) VALUES (1,'admin'),(2,'user');
 replace INTO user_role(id,user_id,role_id) VALUES (1,1,1),(2,2,2);
