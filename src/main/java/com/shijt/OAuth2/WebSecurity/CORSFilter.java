06969c3256b99f0710fa1bddc5169d7680638d95
jiantian.shi <jiantian.shi@gmail.com>
2018-10-31 21:02:21 +0800

    添加跨域配置

---
+++ src/main/java/com/shijt/OAuth2/WebSecurity/CORSFilter.java
@@ -0,0 +1,46 @@
+package com.shijt.OAuth2.WebSecurity;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 跨域配置
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request= (HttpServletRequest) req;
+        HttpServletResponse response= (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Methods","*");
+        //预检请求的结果可以被缓存的最大秒数
+        response.setHeader("Access-Control-Max-Age","3600");
+        response.setHeader("Access-Control-Allow-Headers","*");
+
+        if("OPITIONS".equalsIgnoreCase(request.getMethod())){
+            response.setStatus(HttpServletResponse.SC_OK);
+        }else{
+            chain.doFilter(req,res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
