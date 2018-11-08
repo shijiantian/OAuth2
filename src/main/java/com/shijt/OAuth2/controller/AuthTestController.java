3624f7b47b9cb9ab9f0b185ed6a0172e1e77a5bc
jiantian.shi <jiantian.shi@gmail.com>
2018-11-08 21:27:03 +0800

    格式调整

--- src/main/java/com/shijt/OAuth2/controller/AuthTestController.java
+++ src/main/java/com/shijt/OAuth2/controller/AuthTestController.java
@@ -6,11 +6,10 @@ import com.shijt.OAuth2.vo.ControllerResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Api(value = "登录接口测试")
 @RestController
@@ -19,12 +18,12 @@ public class AuthTestController {
 
     @Autowired
     private TestMysqlService mysqlService;
-    @Autowired
 
     @ApiOperation(value = "登录测试",notes = "测试登录并返回所以用户信息")
     @RequestMapping(value = "login",method = RequestMethod.GET)
     public Object helloWorld(){
-        List<String> studentName=mysqlService.getUserName();
+//        List<String> studentName=mysqlService.getUserName();
+        String studentName= SecurityContextHolder.getContext().getAuthentication().getName();
         return new ControllerResult(studentName);
     }
 }
