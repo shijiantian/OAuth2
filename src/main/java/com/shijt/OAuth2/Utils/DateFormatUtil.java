bb9761ad4d28f5bd14e9a30f7d1a239af656ce19
jiantian.shi <jiantian.shi@gmail.com>
2018-11-27 20:32:10 +0800

    添加date格式化函数

--- src/main/java/com/shijt/OAuth2/Utils/DateFormatUtil.java
+++ src/main/java/com/shijt/OAuth2/Utils/DateFormatUtil.java
@@ -3,10 +3,11 @@ package com.shijt.OAuth2.Utils;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateFormatUtil {
-    public static Date str2Date(String dateStr){
+    public static Date str2DayDate(String dateStr){
         DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
         Date parseDate=null;
         try{
@@ -15,5 +16,23 @@ public class DateFormatUtil {
             e.printStackTrace();
         }
         return parseDate;
+    }
+
+    public static String date2DayStr(Date date){
+        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
+        return  dateFormat.format(date);
+    }
+
+    public static String date2MonthStr(Date date){
+        DateFormat dateFormat=new SimpleDateFormat("yyy-MM");
+        return dateFormat.format(date);
+
+    }
+
+    public static Date getPreviousMonth(Date date,int preCount){
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH,preCount);
+        return calendar.getTime();
     }
 }
