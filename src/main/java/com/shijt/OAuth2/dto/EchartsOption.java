fa0de39302317d96c4498d4efd5e832dd2ccfabf
jiantian.shi <jiantian.shi@gmail.com>
2018-11-27 20:27:05 +0800

    创建echars选项类

---
+++ src/main/java/com/shijt/OAuth2/dto/EchartsOption.java
@@ -0,0 +1,53 @@
+package com.shijt.OAuth2.dto;
+
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+
+import java.io.Serializable;
+
+public class EchartsOption implements Serializable {
+    private Object title;
+    private Object legend;
+    private Object xAxis;
+    private Object yAxis;
+    private Object series;
+
+    public Object getTitle() {
+        return title;
+    }
+
+    public void setTitle(Object title) {
+        this.title = title;
+    }
+
+    public Object getLegend() {
+        return legend;
+    }
+
+    public void setLegend(Object legend) {
+        this.legend = legend;
+    }
+
+    public Object getxAxis() {
+        return xAxis;
+    }
+
+    public void setxAxis(Object xAxis) {
+        this.xAxis = xAxis;
+    }
+
+    public Object getyAxis() {
+        return yAxis;
+    }
+
+    public void setyAxis(Object yAxis) {
+        this.yAxis = yAxis;
+    }
+
+    public Object getSeries() {
+        return series;
+    }
+
+    public void setSeries(Object series) {
+        this.series = series;
+    }
+}
