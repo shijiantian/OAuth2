0056e674e2e3ad7b42775b6dd3983789c6ea49ac
jiantian.shi <jiantian.shi@gmail.com>
2019-01-22 17:18:37 +0800

    添加转换string为float的函数

--- src/main/java/com/shijt/OAuth2/Utils/DataConversionUtil.java
+++ src/main/java/com/shijt/OAuth2/Utils/DataConversionUtil.java
@@ -18,6 +18,19 @@ public class DataConversionUtil {
 
     }
 
+    public static float str2Float(String value,float defaultValue){
+        if(StringUtils.isEmpty(value)){
+            return defaultValue;
+        }else{
+            try {
+                return Float.valueOf(value);
+            }catch (NumberFormatException e){
+                e.printStackTrace();
+                return defaultValue;
+            }
+        }
+    }
+
     public static long str2Long(String value,long defaultValue){
         if(StringUtils.isEmpty(value)){
             return defaultValue;
