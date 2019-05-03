66a7c827c19accaa215d40d0d141b6b6361ad311
jiantian.shi <jiantian.shi@gmail.com>
2019-05-03 16:06:18 +0800

    修改注释

--- src/main/java/com/shijt/OAuth2/aop/ExpenseHistoryAspect.java
+++ src/main/java/com/shijt/OAuth2/aop/ExpenseHistoryAspect.java
@@ -15,13 +15,13 @@ public class ExpenseHistoryAspect {
     @Pointcut("execution(* com.shijt.OAuth2.services.impl.ExpenseHistoryServiceImpl.*(..))")
     public void expenseHistory(){}
 
-    //执行切点之前
+    //切点执行之前切入
     @Before(value = "expenseHistory()")
     public void doBefore(JoinPoint joinPoint){
         System.out.println("-----start to query ExpenseHistory table !-------");
     }
 
-    //切点返回之后
+    //切点返回之后切入（抛出异常不会切入,@After注解才会在抛出异常之后依旧切入）
     @AfterReturning(pointcut = "expenseHistory()",returning = "resultObject")
     public void doAfterReturning(Object resultObject){
         System.out.println("----end the Query------"+resultObject+"-----");
