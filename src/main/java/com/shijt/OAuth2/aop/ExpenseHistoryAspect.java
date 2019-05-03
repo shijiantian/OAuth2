e63fcaaa5bbfed742decaf0e7de5b8dd9c6406e1
jiantian.shi <jiantian.shi@gmail.com>
2019-05-03 16:32:27 +0800

    添加切面处理函数

--- src/main/java/com/shijt/OAuth2/aop/ExpenseHistoryAspect.java
+++ src/main/java/com/shijt/OAuth2/aop/ExpenseHistoryAspect.java
@@ -1,10 +1,7 @@
 package com.shijt.OAuth2.aop;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -17,14 +14,37 @@ public class ExpenseHistoryAspect {
 
     //切点执行之前切入
     @Before(value = "expenseHistory()")
-    public void doBefore(JoinPoint joinPoint){
+    public void doBefore(){
         System.out.println("-----start to query ExpenseHistory table !-------");
     }
 
-    //切点返回之后切入（抛出异常不会切入,@After注解才会在抛出异常之后依旧切入）
+    //@Around封装了切点的处理过程，可编写贯穿函数前后的逻辑,例如计算函数执行时间
+    //本函数需要有返回值，否则切点的返回结果会为空
+    @Around(value = "expenseHistory()")
+    public Object doAround(ProceedingJoinPoint joinpoint) throws Throwable{
+        long startTime=System.currentTimeMillis();
+        Object result=joinpoint.proceed();
+        long endTime=System.currentTimeMillis();
+        System.out.println("-------执行耗时:"+(endTime-startTime));
+        return result;
+    }
+
+    //切点成功返回之后切入（抛出异常不会切入）
+    //returning注入切点返回值
     @AfterReturning(pointcut = "expenseHistory()",returning = "resultObject")
     public void doAfterReturning(Object resultObject){
-        System.out.println("----end the Query------"+resultObject+"-----");
+        System.out.println("----function exec succeed------"+resultObject+"-----");
+    }
+
+    //切点之后执行(无论是否抛出异常)
+    @After(value = "expenseHistory()")
+    public void doAfter(){
+        System.out.println("----function exec end-------");
+    }
+
+    @AfterThrowing(pointcut = "expenseHistory()",throwing = "exceptionObject")
+    public void doAfterThrowing(Exception exceptionObject){
+        System.out.println("----get exception----"+exceptionObject.getMessage());
     }
 
 }
