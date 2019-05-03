f11d7b4f16c1c5d4f4729368abb81100f9cdc572
jiantian.shi <jiantian.shi@gmail.com>
2019-05-03 15:58:07 +0800

    为ExpenseHistoryServiceImpl添加aop

---
+++ src/main/java/com/shijt/OAuth2/aop/ExpenseHistoryAspect.java
@@ -0,0 +1,30 @@
+package com.shijt.OAuth2.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExpenseHistoryAspect {
+
+    //将ExpenseHistoryServiceImpl类中的所有方法定义为切入点
+    @Pointcut("execution(* com.shijt.OAuth2.services.impl.ExpenseHistoryServiceImpl.*(..))")
+    public void expenseHistory(){}
+
+    //执行切入点之前
+    @Before(value = "expenseHistory()")
+    public void doBefore(JoinPoint joinPoint){
+        System.out.println("-----start to query ExpenseHistory table !-------");
+    }
+
+    //切入点返回之后
+    @AfterReturning(pointcut = "expenseHistory()",returning = "resultObject")
+    public void doAfterReturning(Object resultObject){
+        System.out.println("----end the Query------"+resultObject+"-----");
+    }
+
+}
