dceaa4dd8c842d4a96f282a47eb953fd962082b3
jiantian.shi <jiantian.shi@gmail.com>
2019-05-03 16:04:04 +0800

    修改注释

--- src/main/java/com/shijt/OAuth2/aop/ExpenseHistoryAspect.java
+++ src/main/java/com/shijt/OAuth2/aop/ExpenseHistoryAspect.java
@@ -11,17 +11,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class ExpenseHistoryAspect {
 
-    //将ExpenseHistoryServiceImpl类中的所有方法定义为切入点
+    //将ExpenseHistoryServiceImpl类中的所有方法定义为切点
     @Pointcut("execution(* com.shijt.OAuth2.services.impl.ExpenseHistoryServiceImpl.*(..))")
     public void expenseHistory(){}
 
-    //执行切入点之前
+    //执行切点之前
     @Before(value = "expenseHistory()")
     public void doBefore(JoinPoint joinPoint){
         System.out.println("-----start to query ExpenseHistory table !-------");
     }
 
-    //切入点返回之后
+    //切点返回之后
     @AfterReturning(pointcut = "expenseHistory()",returning = "resultObject")
     public void doAfterReturning(Object resultObject){
         System.out.println("----end the Query------"+resultObject+"-----");
