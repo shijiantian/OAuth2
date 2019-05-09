ddd2c9d33a681f23ec14e51c526bd33d2ff8f73d
jiantian.shi <jiantian.shi@gmail.com>
2019-05-09 17:05:18 +0800

    添加redis缓存key生成策略

--- src/main/java/com/shijt/OAuth2/config/RedisConfig.java
+++ src/main/java/com/shijt/OAuth2/config/RedisConfig.java
@@ -1,8 +1,9 @@
 package com.shijt.OAuth2.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -11,9 +12,11 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.lang.reflect.Method;
+
 @Configuration
 //@ComponentScan("com.shijt.OAuth2.config")
-public class RedisConfig {
+public class RedisConfig extends CachingConfigurerSupport {
 
     @Value("${spring.redis.host}")
     private String host;
@@ -42,4 +45,23 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(new GenericToStringSerializer<>(Object.class));
         return redisTemplate;
     }
+
+    //重写redis缓存key的生成策略
+    @Override
+    @Bean
+    public KeyGenerator keyGenerator(){
+        return new KeyGenerator(){
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(":" + method.getName());
+                for (Object obj : params) {
+                    sb.append(":" + (null == obj ? "null" : obj.toString()));
+                }
+                return sb.toString();
+            }
+        };
+    }
+
 }
