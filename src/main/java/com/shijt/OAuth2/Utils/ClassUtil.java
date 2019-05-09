008f875051b1a7ce8d4a5ac2a7b01cf70544a96d
jiantian.shi <jiantian.shi@gmail.com>
2019-05-09 15:27:41 +0800

    添加类操作工具

---
+++ src/main/java/com/shijt/OAuth2/Utils/ClassUtil.java
@@ -0,0 +1,28 @@
+package com.shijt.OAuth2.Utils;
+
+import java.io.*;
+
+public class ClassUtil {
+    public static boolean CanonicalNameIsTheSame(Object a,Class<?> b){
+        String nameOfA=a.getClass().getCanonicalName();
+        String nameOfB=b.getCanonicalName();
+        return nameOfA.equals(nameOfB);
+    }
+
+    public static <T> T serializableObjectCast(Object object){
+        Object result=null;
+        try {
+            final PipedOutputStream pipedOutputStream=new PipedOutputStream();
+            final PipedInputStream  pipedInputStream =new PipedInputStream(pipedOutputStream);
+            final ObjectOutputStream objectOutputStream=new ObjectOutputStream(pipedOutputStream);
+            final ObjectInputStream  objectInputStream =new ObjectInputStream(pipedInputStream);
+            objectOutputStream.writeObject(object);
+            objectOutputStream.flush();
+            result=objectInputStream.readObject();
+        }catch (IOException |ClassNotFoundException e ){
+            e.printStackTrace();
+        }
+        return (T) result;
+    }
+
+}
